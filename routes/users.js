1b133d96a6c1e91047df4e0e21a10a13e7ae6b16
ihssenedr <drinetn@outlook.com>
2022-07-11 16:27:41 +0200

    add new user

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send({'name': 'Alex', "role": "Software Engineer"});
+  res.send([{'name': 'Alex', "role": "Software Engineer"}, {'name': 'Bob', "role": "Software Engineer"}]);
 });
 
 module.exports = router;
